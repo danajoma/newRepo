4bba7df4becfc7903876fb3f1893134164cf977f
Dana <jomaadana75@gmail.com>
2026-08-05 18:18:25 +0300

    editing

--- app.js
+++ app.js
@@ -47,8 +47,14 @@ app.use("/submission", submissionRouter);
 
 
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "test") {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
 
 
