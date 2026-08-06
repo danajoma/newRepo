5a88d4c9b9fbd8f4c170d558d62416c8470e28dc
Dana <jomaadana75@gmail.com>
2026-08-06 12:13:18 +0300

    editing

---
+++ tests/auth.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Authentication", () => {
+
+    test("GET /health should return server status", async () => {
+
+        const res = await request(app).get("/health");
+
+        expect(res.statusCode).toBe(200);
+
+        expect(res.body).toEqual({
+            status: "Server is running"
+        });
+
+    });
+
+});
+
+
